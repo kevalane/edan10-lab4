7acff2024fe6b95d1abac92819af226f94bc9fc6
Henrik Vester <henrik645@gmail.com>
2023-12-01 10:40:09 +0100

    Fix indentation

--- src1/project/src/MyTree.java
+++ src1/project/src/MyTree.java
@@ -9,76 +9,76 @@ import tree.MyNode;
  * @see MyNode
  */
 public class MyTree {
-  private MyNode root;
-  private int test;
+    private MyNode root;
+    private int test;
 
-  /**
-   * Class constructor.
-   */
-  public MyTree() {
-      test = 3;
-  }
-
-  /**
-   * Return a reference to the root in this tree.
-   *
-   * @return Reference to the root node.
-   */
-  public MyNode getRoot() {
-    test++;
-    return root;
-  }
-
-  /**
-   * Inserts a node in this tree. The tree is sorted by the node values.  If a
-   * node with the same node value is already in the tree nothing happens.
-   *
-   * @param node The node to insert in this tree.
-   * @return True if node is inserted, false otherwise.
-   */
-  public boolean insert(MyNode node) {
-    if(root == null) {
-      root = node;
-      return true;
+    /**
+     * Class constructor.
+     */
+    public MyTree() {
+        test = 3;
     }
-    MyNode iter = root;
 
-    while(true) {
-      if(iter.getValue() < node.getValue()) {
-        if(iter.getRightChild() != null) {
-          iter = iter.getRightChild();
-        }
-        else {
-          iter.setRightChild(node);
-          return true;
-        }
-      }
-      else if(iter.getValue() > node.getValue()) {
-        if(iter.getLeftChild() != null) {
-          iter = iter.getLeftChild();
-        }
-        else {
-          iter.setLeftChild(node);
-          return true;
-        }
-      }
-      else if(iter.getValue() == node.getValue()) {
-        return false;
-      }
+    /**
+     * Return a reference to the root in this tree.
+     *
+     * @return Reference to the root node.
+     */
+    public MyNode getRoot() {
+        test++;
+        return root;
     }
-  }
 
-  /**
-   * Calculate the number of nodes in this tree. Recursively calculate the
-   * number of nodes in this search tree.
-   *
-   * @return The total number of nodes.
-   */
-  public int cardinal() {
-    if(root == null) {
-      return 0;
+    /**
+     * Inserts a node in this tree. The tree is sorted by the node values.  If a
+     * node with the same node value is already in the tree nothing happens.
+     *
+     * @param node The node to insert in this tree.
+     * @return True if node is inserted, false otherwise.
+     */
+    public boolean insert(MyNode node) {
+        if(root == null) {
+            root = node;
+            return true;
+        }
+        MyNode iter = root;
+
+        while(true) {
+            if(iter.getValue() < node.getValue()) {
+                if(iter.getRightChild() != null) {
+                    iter = iter.getRightChild();
+                }
+                else {
+                    iter.setRightChild(node);
+                    return true;
+                }
+            }
+            else if(iter.getValue() > node.getValue()) {
+                if(iter.getLeftChild() != null) {
+                    iter = iter.getLeftChild();
+                }
+                else {
+                    iter.setLeftChild(node);
+                    return true;
+                }
+            }
+            else if(iter.getValue() == node.getValue()) {
+                return false;
+            }
+        }
     }
-    return root.calcSize();
-  }
+
+    /**
+     * Calculate the number of nodes in this tree. Recursively calculate the
+     * number of nodes in this search tree.
+     *
+     * @return The total number of nodes.
+     */
+    public int cardinal() {
+        if(root == null) {
+            return 0;
+        }
+        return root.calcSize();
+    }
 }
 
